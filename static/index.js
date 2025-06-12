19f6f4be9584edd8d63c2f24b97e452a4523e4d8
elunico <elunico@users.noreply.github.com>
2025-06-12 15:31:40 -0400

    update styles

--- static/index.js
+++ static/index.js
@@ -3,19 +3,59 @@ let button = document.querySelector("#search-button");
 button.addEventListener("click", doSubmission);
 console.log("done");
 
+let greenInputs = [];
+let yellowInputs = [];
+let grayInput = document.querySelector("#gray-in");
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    for (let i = 1; i <= 5; i++) {
+      yellowInputs.push(document.querySelector(`#yellow${i}-in`));
+      greenInputs.push(document.querySelector(`#green${i}-in`));
+    }
+
+    for (let idx = 0; idx < 5; idx++) {
+      let inelt = greenInputs[idx];
+      inelt.addEventListener("input", function () {
+        // if (this.value.length > 1) {
+        //   this.value = this.value.charAt(this.value.length - 1);
+        // }
+        if (this.value.length == 1) {
+          if (this.value == " ") {
+            this.value = "";
+          }
+          greenInputs[idx + 1].focus();
+        }
+      });
+
+      inelt.addEventListener("keypress", function () {
+        if (event.key == "Enter") button.click();
+      });
+    }
+
+    for (let y of yellowInputs) {
+      y.addEventListener("keypress", function () {
+        if (event.key == "Enter") button.click();
+      });
+    }
+
+    grayInput.addEventListener("keypress", function () {
+      if (event.key == "Enter") button.click();
+    });
+  }
+};
+
 function doSubmission() {
   let greenIns = [];
   let yellowIns = [];
   for (let i = 1; i <= 5; i++) {
-    let yi = document.querySelector(`#yellow${i}-in`);
-    let gi = document.querySelector(`#green${i}-in`);
+    let yi = yellowInputs[i - 1];
+    let gi = greenInputs[i - 1];
     greenIns.push(gi.value);
     yellowIns.push(yi.value);
   }
-  console.log(greenIns, yellowIns);
 
-  let grayIn = document.querySelector("#gray-in");
-  let grays = grayIn.value;
+  let grays = grayInput.value;
 
   let url = URL.parse("search", window.location.toString());
   for (let i = 0; i < 5; i++) {
@@ -23,8 +63,6 @@ function doSubmission() {
     url.searchParams.set(`y${i + 1}`, yellowIns[i]);
   }
   url.searchParams.set("grays", grays);
-
-  console.log(url);
 
   fetch(url)
     .then((resp) => resp.json())
